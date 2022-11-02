349b8bc6e258779255427f69d3a02ebbc2a42220
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-11-02 18:22:48 +0530

    more code

--- src/components/Display/Firstly.js
+++ src/components/Display/Firstly.js
@@ -1,22 +1,19 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import classes from "./Firstly.module.css";
 import Footer from "./Footer.js";
 import Form from "./Form/Form.js";
 import Stocks from "./Stocks/Stocks.js";
 const Firstly = () => {
   const [data, setData] = useState([{}]);
-  function getData(stdata){
+  function getData(stdata) {
     setData(stdata);
-    console.log(stdata);
   }
-  console.log(typeof(data));
+  console.log(typeof data);
   return (
     <React.Fragment>
       <div className={`${classes.card}`}>
-      <h2>Hey! Welcome here ツ</h2>
-        <Form 
-          parentsgetData={getData}
-        />
+        <h2>Hey! Welcome here ツ</h2>
+        <Form parentsgetData={getData} />
       </div>
       <Stocks />
       <Footer />
