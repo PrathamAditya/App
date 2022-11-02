12ff96782ec3da92df6a61ffa1972b2a5a118a63
Sameer Goyal <sameergoyal125@gmail.com>
2022-11-02 18:22:26 +0530

    more

--- src/components/Display/Form/Form.js
+++ src/components/Display/Form/Form.js
@@ -29,30 +29,6 @@ const Form = (props) => {
     }, [count])
   
   return (
-<<<<<<< HEAD
-    <div className={classes["apply-box"]}>
-      {" "}
-      <h2>Hey! Welcomeツ</h2>
-      <form onSubmit={submitHandler}>
-        <div className={classes["user-box"]}>
-          <input type="text" name="" required=""></input>
-          <label>Stock Name:</label>
-        </div>
-        <div className={classes["user-box"]}>
-          <input type="date" name="" required=""></input>
-          <label>Choose Start date:</label>
-        </div>
-        <div className={classes["two-buttons-main"]}>
-          <div className={classes["two-buttons-outer"]}>
-            <button className={classes["two-buttons-inner"]}>
-              Stock Price
-            </button>
-          </div>
-          <div className={classes["two-buttons-outer"]}>
-            <button className={classes["two-buttons-inner"]}>Indicator</button>
-          </div>
-        </div>
-=======
     <React.Fragment>
       <form onSubmit={submitHandler} className={classes.mainclass}>
         <label type="text" className={classes.stockname}>
@@ -61,7 +37,6 @@ const Form = (props) => {
         <input type="text" onChange={event => setStock(event.target.value)} value={stock}></input>
         <input type="date" className={classes.startdate} onChange={event => setDate(event.target.value)} value={date}></input>
         <input type="submit" onClick={() => setCount(count + 1)} value="Submit"></input>
->>>>>>> 94895efd6f2ac3edffc5c2f513f53b35df6ec3c8
       </form>
       {/* <Card 
         stockdata={data}
