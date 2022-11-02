6c2e957f9b3d52d5944496f9c422726d43b8ebb7
Pratham Aditya Salhotra <prathamadityasalhotra@gmail.com>
2022-11-02 12:10:03 +0530

    baseProject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Firstly from "./components/Display/Firstly.js";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Firstly></Firstly>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Display/Firstly.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Firstly.css";
+import Footer from "./Footer.js";
+
+const Firstly = (props) => {
+  return (
+    <React.Fragment>
+      <h2>Hey! Welcome here ツ</h2>
+      <Footer></Footer>
+    </React.Fragment>
+  );
+};
+
+export default Firstly;
