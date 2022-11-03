9341995cf168b3bf43d19bdfe654e0b7278997f5
Sameer Goyal <sameergoyal125@gmail.com>
2022-11-03 13:41:34 +0530

    added price functionality

--- server/server.py
+++ server/server.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "API For Stock info"
-@app.route("/<name>&&date=<date>")
-def get_stock(name,date):
+@app.route("/<name>&&start_date=<start_date>&&end_date=<end_date>")
+def get_stock(name,start_date,end_date):
+    startDate=""
+    endDate = end_date
+    if start_date == "none":
+        startDate = str(int(startDate[:4])-1) + startDate[4:]
+    else:
+         startDate = start_date
     fulldata=yfinance.Ticker(name).info
-    data={"name":fulldata["shortName"],"info":fulldata["longBusinessSummary"]}
+    price=(yfinance.Ticker(name).history(start=startDate,end=endDate))
+    print(price.to_json())
+    data={"name":fulldata["shortName"],"info":fulldata["longBusinessSummary"],"price":price.to_json()}
+
     return data
 
 if __name__ == "__main__":
